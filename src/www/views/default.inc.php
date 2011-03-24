4e2f7fa72b74595b4306055c555b81a156844ca7
Bendik Brenne <bendik.brenne@freecode.no>
2011-03-24 16:13:48 +0100

    bugfix

--- src/www/views/default.inc.php
+++ src/www/views/default.inc.php
@@ -9,6 +9,7 @@ $list = $epguide->getEpisodes( $time_start, $time_end, 10 );
 foreach($list as $date => $episodes) {
 	foreach($episodes as $ep) {
 		$diff = distanceOfTimeInWords( time(), $ep['time'] );
+		$ep['no'] = sprintf('%1$02dx%2$02d', $ep['season'], $ep['episode']);
 		echo '
 		<tr>
 			<td width="110">' . $diff . '</td>
